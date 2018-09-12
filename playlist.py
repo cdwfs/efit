854c8d8aa7c40342e824332d9503daadbb1952a7
Cort <1944792+cdwfs@users.noreply.github.com>
2018-09-12 05:11:31 -0700

    Added basic argparse support to playlist.py

--- playlist.py
+++ playlist.py
@@ -1,9 +1,16 @@
+import argparse
 import efit
 import sys
 
 if __name__ == "__main__":
-    lib_filename = sys.argv[1]
-    playlist_name = sys.argv[2]
-    out_filename = sys.argv[3]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--library", "-l", required=True, help="iTunes library file [.xml, .bz2, .pkl]")
+    parser.add_argument("--playlist", "-p", required=True, help="name of playlist to extract from library")
+    parser.add_argument("--output", "-o", required=True, help="output playlist file [.m3u8]")
+    args = parser.parse_args()
+
+    lib_filename = args.library
+    playlist_name = args.playlist
+    out_filename = args.output
     lib = efit.load_library_from_file(lib_filename)
     efit.export_playlist(lib, playlist_name, out_filename)
