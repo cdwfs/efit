f7eb0be43611c02a1b1596c87caf3885e3e0053d
Cort <1944792+cdwfs@users.noreply.github.com>
2021-08-04 15:57:12 -0700

    Update playlist names

--- export_all_playlists.bat
+++ export_all_playlists.bat
@@ -1,3 +1,3 @@
 @echo off
 REM Usage export_all_playlists Library.xml
-python playlist.py -L %1 Bedtime Chiptunes Christmas Comprehensive Definitive Demoscene "For The Ladies" "Girls Top 10" Homemade La LM Longform Potatoes "Proving Ground" Repertoire Show Sleepy Trance "Video Game Music" Zenith
+python playlist.py -L %1 Bedtime Chiptunes Christmas Comprehensive Definitive Demoscene "For The Ladies" "Girls Top 10" Homemade La LM Longform "New Hotness" Perspective Potatoes "Proving Ground" Repertoire Show Sleepy Trance Zenith
