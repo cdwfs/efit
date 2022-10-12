5e417abeb31352346693a7bc6bea38d34fe6171b
Cort <1944792+cdwfs@users.noreply.github.com>
2022-10-12 16:02:52 -0700

    ratings.py: print summary of changes at the end of execution

--- ratings.py
+++ ratings.py
@@ -6,6 +6,16 @@ import mutagen.mp4
 import os.path
 import sys
 
+# Total number of files changed
+total_change_count = 0
+# List of changes where an existing non-zero rating was overwritten with a new value.
+# These are worth closer review (vs. cases where the previous rating was zero, and the
+# rating is assumed to be new); every once in a while a rating is changed accidentally,
+# and this is a chance to catch and correct that.
+changed_existing_ratings = []
+# List of songs with invalid/inaccessible paths
+invalid_song_path_errors = []
+
 def get_itunes_star_rating(song):
     if song.rating_computed:
         return 0
@@ -14,6 +24,8 @@ def get_itunes_star_rating(song):
     return round(song.rating * 5 / 100)
 
 def update_mp3_rating(song_path, itunes_stars):
+    global total_change_count
+    global changed_existing_ratings
     "Assumes song_path is a valid file. itunes_stars is 0-5."
     # Extract current POPM rating, if any.
     max_stars = 5
@@ -26,14 +38,21 @@ def update_mp3_rating(song_path, itunes_stars):
     except IndexError as e: current_popm_stars = 0
     # Update POPM tag with new rating if necessary
     if itunes_stars != current_popm_stars:
-        print(f"\r{' '*32}\r{song.name}: {current_popm_stars} -> {itunes_stars} stars")
+        change_type = "NEW" if current_popm_stars == 0 else "CHANGED"
+        change_summary = f"{song.name}: {current_popm_stars} -> {itunes_stars} stars"
+        if current_popm_stars != 0:
+            changed_existing_ratings.append(change_summary)
+        print(f"\r{' '*32}\r{change_summary} [{change_type}]")
         id3 = mutagen.id3.ID3(song_path)
         rating = round(itunes_stars * 255 / max_stars)
         frame = mutagen.id3.POPM(email="quodlibet@lists.sacredchao.net", rating=rating, count=0)
         id3.setall("POPM", [frame])
         id3.save(song_path, v1=mutagen.id3.ID3v1SaveOptions.REMOVE)
+        total_change_count += 1
 
 def update_m4a_rating(song_path, itunes_stars):
+    global total_change_count
+    global changed_existing_ratings
     "Assumes song_path is a valid file. itunes_stars is 0-5."
     # There is no universal standard for per-file ratings in M4A files.
     # Different players have adopted different conventions in the meantime.
@@ -54,7 +73,11 @@ def update_m4a_rating(song_path, itunes_stars):
         print(f"\r{' '*32}\r{song_path}: tag issue!")
         sys.exit(-1)
     if current_rating != itunes_stars:
-        print(f"\r{' '*32}\r{song.name}: {current_rating} -> {itunes_stars} stars")
+        change_type = "NEW" if current_rating == 0 else "CHANGED"
+        change_summary = f"{song.name}: {current_rating} -> {itunes_stars} stars"
+        if current_rating != 0:
+            changed_existing_ratings.append(change_summary)
+        print(f"\r{' '*32}\r{change_summary} [{change_type}]")
         tags["rate"] = [str(round(itunes_stars * 100 / 5))]
         changed_tag = True
     # Various extended atoms may or may not be used, but store their values as
@@ -74,6 +97,7 @@ def update_m4a_rating(song_path, itunes_stars):
             changed_tag = True
     if changed_tag:
         tags.save(song_path)
+        total_change_count += 1
 
 if __name__ == "__main__":
     lib_filename = sys.argv[1]
@@ -87,12 +111,15 @@ if __name__ == "__main__":
                 song_path = efit.get_song_path(song)
                 if not os.path.isfile(song_path):
                     print(f"\r{' '*32}\r{song.location}: file not found")
+                    invalid_song_path_errors.append(song.location)
                     continue
             except TypeError:
                 print(f"\r{' '*32}\rpath error for {song.location}")
+                invalid_song_path_errors.append(song.location)
                 continue
             except ValueError:
                 print(f"\r{' '*32}\rpath error for {song.location}")
+                invalid_song_path_errors.append(song.location)
                 continue
             itunes_stars = get_itunes_star_rating(song)
             suffix = song_path[-4:].lower()
@@ -104,3 +131,11 @@ if __name__ == "__main__":
                 print(f"\r{' '*32}\r{song.name}: skipping unrecognized file type")
 
         print(f"\r{i_song:6}/{song_count:6} songs processed", end="")
+    # Print summary of work & any highlights
+    print(f"\nJob's done! {total_change_count} files changed.")
+    print(f"{len(changed_existing_ratings)} existing ratings changed.")
+    for change in changed_existing_ratings:
+        print(f"- {change}")
+    print(f"{len(invalid_song_path_errors)} songs with invalid paths.")
+    for path in invalid_song_path_errors:
+        print(f"- {path}")
