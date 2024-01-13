7726d3f273ccbec1323eea544efc17642c4d1f47
Cort <1944792+cdwfs@users.noreply.github.com>
2024-01-13 10:19:59 -0800

    libpytunes now calls open() internally, and expects a filename as input. BZ2 no longer supported.

--- efit/efit.py
+++ efit/efit.py
@@ -16,11 +16,7 @@ def load_library_from_file(filename):
     """Loads an iTunes library from either an XML file (exported from iTunes) or a pickled .pkl file."""
     base, ext = os.path.splitext(filename)
     if ext == ".xml":
-        with open(filename, 'rb') as fp:
-            lib = libpytunes.Library(fp)
-    elif ext == ".bz2":
-        with bz2.open(filename) as fbz2:
-            lib = libpytunes.Library(fbz2)
+        lib = libpytunes.Library(filename)
     elif ext == ".pkl":
         lib_file = open(filename, "rb")
         lib = pickle.load(lib_file)
